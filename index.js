d021921d81838f478b42d3d24c91614f3356527e
Yechiel K <chilikkal@gmail.com>
2017-07-10 12:53:19 -0400

    got working method for obtaining a list of all the user's repos

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
-function markRepos() {
-  const repo = 'achasveachas/javascript-fetch-lab'
-  const title = document.getElementById('title').value
-  const body = document.getElementById('body').value
-  const postData = {
-    title: title,
-    body: body
+let repoList = []
+
+function handleMarkRepos() {
+  if (confirm("Are you sure you want to mark your repos private?\nThis step can not be undone easily.")) {
+
+    getRepoList(1)
   }
-  fetch(`https://api.github.com/repos/${repo}/issues`, {
-    method: 'post',
-    body: JSON.stringify(postData),
-    headers: {
-      Authorization: `Token ${getToken()}`
-    }
-  }).then(console.log('Issue Created')).then(getIssues())
 }
 
+function getRepoList(page) {
+
+  fetch(`https://api.github.com/user/repos?page=${page}&per_page=100`, {
+    method: 'get',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+  .then(res => res.json())
+  .then((json) => {
+    if (json.length < 1) {
+      filterRepoList()
+    } else {
+      repoList = repoList.concat(json)
+      getRepoList(page + 1)
+    }
+  })
+}
+
+function filterRepoList() {
+  console.log(repoList.length);
+}
+
+function getUsername() {
+  // Return your GitHub username below
+  return 'achasveachas'
+}
 
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
+  // Go to https://github.com/settings/tokens and get a token, make sure to delete it before commiting
   return ''
 }
