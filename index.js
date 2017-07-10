ace248db7f4640523ae374095b55743e8e7b3d37
Yechiel K <chilikkal@gmail.com>
2017-07-10 12:14:28 -0400

    cleaned up index.js

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-function getIssues() {
-  const repo = 'achasveachas/javascript-fetch-lab'
-  fetch(`https://api.github.com/repos/${repo}/issues`).then(console.log('Got Issues')).then(res => res.json()).then(json => showIssues(json))
-}
-
-function showIssues(json) {
-  var template = Handlebars.compile(document.getElementById('issues-template').innerHTML)
-  var issues = template(json)
-  document.getElementById('results').innerHTML = issues
-}
-
-function createIssue() {
+function markRepos() {
   const repo = 'achasveachas/javascript-fetch-lab'
   const title = document.getElementById('title').value
   const body = document.getElementById('body').value
@@ -26,22 +15,6 @@ function createIssue() {
   }).then(console.log('Issue Created')).then(getIssues())
 }
 
-function showResults(json) {
-  var template = Handlebars.compile(document.getElementById('repo-template').innerHTML)
-  var result = template(json)
-  document.getElementById('results').innerHTML = result
-}
-
-function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
-  fetch(`https://api.github.com/repos/${repo}/forks`, {
-    method: 'post',
-    headers: {
-      Authorization: `Token ${getToken()}`
-    }
-  }).then(res => res.json()).then(json => showResults(json))
-}
 
 function getToken() {
   //change to your token to run in browser, but set
